fcd5ffdbcaafc1bde8da0ce8dcf8b72b51994f2c
Gustavo <gustavo.antunes.rocha@outlook.com>
2020-11-29 18:21:10 -0300

    Criação inicial do sistema

---
+++ oauth/src/test/java/br/com/asw/si/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.asw.si.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
