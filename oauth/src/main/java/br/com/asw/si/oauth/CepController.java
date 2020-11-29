b7fad5868e861918e780c05bbff73888831a635b
Gustavo <gustavo.antunes.rocha@outlook.com>
2020-11-29 20:49:16 -0300

    Adicionado input para consulta pelo cep

--- oauth/src/main/java/br/com/asw/si/oauth/CepController.java
+++ oauth/src/main/java/br/com/asw/si/oauth/CepController.java
@@ -3,7 +3,7 @@ package br.com.asw.si.oauth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -12,8 +12,8 @@ public class CepController {
 	RestTemplate restTemplate;
 	private String endpoint = "https://viacep.com.br/ws/";
 	
-	@GetMapping(path="/consultaCep/{cep}")
-	public ResponseEntity<EnderecoApi> consultaCep(@PathVariable String cep) {
+	@GetMapping(path="/consultaCep")
+	public ResponseEntity<EnderecoApi> consultaCep(@RequestParam String cep) {
 		restTemplate = new RestTemplate();
 		final EnderecoApi responseBody = restTemplate.getForObject(
 				endpoint + cep + "/json", EnderecoApi.class);
