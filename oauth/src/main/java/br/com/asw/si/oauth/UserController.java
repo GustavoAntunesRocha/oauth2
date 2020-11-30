173bbee00e6bf36c2118096267a8d685b0104eaa
Gustavo <gustavo.antunes.rocha@outlook.com>
2020-11-29 21:05:33 -0300

    Acesso sem autenticar

---
+++ oauth/src/main/java/br/com/asw/si/oauth/UserController.java
@@ -0,0 +1,19 @@
+package br.com.asw.si.oauth;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+}
